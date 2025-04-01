6395449cb8b730aa558d9c0dc0fe72b9e5d20500
Canmi21 <9997200@qq.com>
2025-04-01 22:10:57 +0800

    add: depens fix stx

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,28 @@ use openjlc::cli::get_input_file_path;
 use openjlc::log;
 use openjlc::extractor::extract_zip_to_temp;
 use openjlc::identifier::{identify_eda_files, EDATool};
-use std::sync::Mutex;
 use lazy_static::lazy_static;
+use std::sync::Mutex;
+use std::fmt;
 
 lazy_static! {
     static ref EDA_TOOL: Mutex<EDATool> = Mutex::new(EDATool::Unknown);
 }
 
+impl fmt::Debug for EDATool {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", match self {
+            EDATool::Altium => "Altium",
+            EDATool::KiCad => "KiCad",
+            EDATool::Unknown => "Unknown",
+        })
+    }
+}
+
 fn main() {
     let temp_dir = get_temp_dir();
     if !temp_dir.exists() {
-        log::log(&format!("! Temp directory not found"));
+        log::log("! Temp directory not found");
         std::fs::create_dir_all(&temp_dir).unwrap();
         log::log(&format!("+ Created temp at {:?}", temp_dir));
     } else {
@@ -32,12 +43,12 @@ fn main() {
 
         match identify_eda_files(&temp_dir) {
             Ok((gerber_file, tool)) => {
-                log::log(&format!("+ Identified {:?} Gerber file: {:?}", tool, gerber_file));
+                log::log(&format!("+ Identified {} Gerber file: {:?}", tool, gerber_file));
                 *EDA_TOOL.lock().unwrap() = tool;
             }
             Err(e) => log::log(&format!("! Failed to identify EDA file: {}", e)),
         }
     } else {
-        log::log(&format!("! No valid file path provided"));
+        log::log("! No valid file path provided");
     }
 }
