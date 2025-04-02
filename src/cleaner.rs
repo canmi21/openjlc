8861e18ce774c8a2210a006857a33f6381b1f040
Canmi21 <9997200@qq.com>
2025-04-02 23:46:05 +0800

    fix: msg

--- src/cleaner.rs
+++ src/cleaner.rs
@@ -10,7 +10,7 @@ pub fn clear_directories() {
         if let Err(e) = fs::remove_dir_all(&temp_dir) {
             log::log(&format!("! Failed to clear temp directory: {}", e));
         } else {
-            log::log(&format!("+ Cleared temp directory: {:?}", temp_dir));
+            log::log(&format!("- Cleared temp directory: {:?}", temp_dir));
         }
     }
 
@@ -18,7 +18,7 @@ pub fn clear_directories() {
         if let Err(e) = fs::remove_dir_all(&target_dir) {
             log::log(&format!("! Failed to clear target directory: {}", e));
         } else {
-            log::log(&format!("+ Cleared target directory: {:?}", target_dir));
+            log::log(&format!("- Cleared target directory: {:?}", target_dir));
         }
     }
 }
