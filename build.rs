2eec24f3539ba9b0f053033a0c1d131192154ef3
Canmi <9997200@qq.com>
2025-04-07 11:22:31 +0800

    fix(x86): link icon err

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" && env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64" {
         let out = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
         println!(
             "cargo:rustc-link-arg-bin=openjlc={}",
