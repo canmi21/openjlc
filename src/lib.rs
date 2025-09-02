a3ed7a50864419574ec95fd7b8b5b43bf157bfcf
Canmi <9997200@qq.com>
2025-09-02 23:35:41 +0800

    refactor: reorder modules for logical grouping in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
+/* src/lib.rs */
+
+pub mod cleaner;
 pub mod cli;
-pub mod log;
 pub mod config;
+pub mod error;
 pub mod extractor;
 pub mod identifier;
-pub mod processor;
-pub mod validator;
 pub mod injector;
+pub mod log;
 pub mod packager;
-pub mod cleaner;
+pub mod processor;
 pub mod reporter;
 pub mod utils;
-pub mod error;
+pub mod validator;
