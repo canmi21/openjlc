cfd4bc31536af96c2d7594378885f5501bb537e3
Canmi <9997200@qq.com>
2025-09-02 23:35:41 +0800

    refactor: refactor report file generation with logging and notification support

--- src/reporter.rs
+++ src/reporter.rs
@@ -1,13 +1,15 @@
+/* src/reporter.rs */
+
+use crate::cleaner::clear_directories;
+use crate::config::{get_report_dir, get_rule_dir, get_target_dir, get_temp_dir};
+use chrono::Utc;
+use notify_rust::Notification;
+use std::env;
 use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
-use std::env;
 use std::time::Duration;
-use chrono::Utc;
 use zip::write::{FileOptions, ZipWriter};
-use crate::config::{get_temp_dir, get_target_dir, get_rule_dir, get_report_dir};
-use crate::cleaner::clear_directories;
-use notify_rust::Notification;
 
 pub fn generate_report(input_zip: PathBuf) {
     let input_zip_name = input_zip.file_name().unwrap().to_owned();
@@ -23,7 +25,6 @@ pub fn generate_report(input_zip: PathBuf) {
                 fs::remove_dir_all(path).unwrap();
             }
         }
-    
     }
     fs::create_dir_all(&report_dir).unwrap();
 
@@ -71,8 +72,13 @@ pub fn generate_report(input_zip: PathBuf) {
         }
     }
 
-    let home_dir = env::var("HOME").or_else(|_| env::var("USERPROFILE")).unwrap_or_else(|_| ".".to_string());
-    let log_dir = PathBuf::from(home_dir).join(".canmi").join("openjlc").join("logs");
+    let home_dir = env::var("HOME")
+        .or_else(|_| env::var("USERPROFILE"))
+        .unwrap_or_else(|_| ".".to_string());
+    let log_dir = PathBuf::from(home_dir)
+        .join(".canmi")
+        .join("openjlc")
+        .join("logs");
     let log_target = report_dir.join("logs");
     fs::create_dir_all(&log_target).unwrap();
     for entry in fs::read_dir(&log_dir).unwrap() {
@@ -97,7 +103,8 @@ pub fn generate_report(input_zip: PathBuf) {
         let path = entry.path();
         let name = path.file_name().unwrap().to_string_lossy();
         if path.is_file() && name != zip_name {
-            let options = FileOptions::<()>::default().compression_method(zip::CompressionMethod::Deflated);
+            let options =
+                FileOptions::<()>::default().compression_method(zip::CompressionMethod::Deflated);
             zip.start_file(name.to_string(), options).unwrap();
             let mut f = File::open(path).unwrap();
             std::io::copy(&mut f, &mut zip).unwrap();
@@ -107,7 +114,8 @@ pub fn generate_report(input_zip: PathBuf) {
                 let sub_path = sub_entry.path();
                 let sub_name = sub_path.file_name().unwrap().to_string_lossy();
                 let relative_name = format!("{}/{}", name, sub_name);
-                let options = FileOptions::<()>::default().compression_method(zip::CompressionMethod::Deflated);
+                let options = FileOptions::<()>::default()
+                    .compression_method(zip::CompressionMethod::Deflated);
                 zip.start_file(relative_name, options).unwrap();
                 let mut f = File::open(sub_path).unwrap();
                 std::io::copy(&mut f, &mut zip).unwrap();
@@ -142,11 +150,20 @@ pub fn generate_report(input_zip: PathBuf) {
         .unwrap();
 
     if cfg!(target_os = "windows") {
-        std::process::Command::new("explorer").arg(report_dir).spawn().unwrap();
+        std::process::Command::new("explorer")
+            .arg(report_dir)
+            .spawn()
+            .unwrap();
     } else if cfg!(target_os = "macos") {
-        std::process::Command::new("open").arg(report_dir).spawn().unwrap();
+        std::process::Command::new("open")
+            .arg(report_dir)
+            .spawn()
+            .unwrap();
     } else {
-        std::process::Command::new("xdg-open").arg(report_dir).spawn().unwrap();
+        std::process::Command::new("xdg-open")
+            .arg(report_dir)
+            .spawn()
+            .unwrap();
     }
 
     std::process::exit(0);
