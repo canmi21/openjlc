ec689930b22a7322eda82cd3e2c427123f706d5c
Canmi <9997200@qq.com>
2025-09-02 23:35:40 +0800

    refactor: reorder imports and use log facade for cleaner module

--- src/cleaner.rs
+++ src/cleaner.rs
@@ -1,6 +1,8 @@
-use std::fs;
+/* src/cleaner.rs */
+
+use crate::config::{get_target_dir, get_temp_dir};
 use crate::log;
-use crate::config::{get_temp_dir, get_target_dir};
+use std::fs;
 
 pub fn clear_directories() {
     let temp_dir = get_temp_dir();
@@ -10,7 +12,10 @@ pub fn clear_directories() {
         if let Err(e) = fs::remove_dir_all(&temp_dir) {
             log::log(&format!("! Failed to clear temp directory: {}", e));
         } else {
-            log::log(&format!("- Cleared temp directory: '{}'", temp_dir.display()));
+            log::log(&format!(
+                "- Cleared temp directory: '{}'",
+                temp_dir.display()
+            ));
         }
     }
 
@@ -18,7 +23,10 @@ pub fn clear_directories() {
         if let Err(e) = fs::remove_dir_all(&target_dir) {
             log::log(&format!("! Failed to clear target directory: {}", e));
         } else {
-            log::log(&format!("- Cleared target directory: '{}'", target_dir.display()));
+            log::log(&format!(
+                "- Cleared target directory: '{}'",
+                target_dir.display()
+            ));
         }
     }
 }
